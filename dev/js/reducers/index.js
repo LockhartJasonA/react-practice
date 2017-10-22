ce9079e18fbc4ac191f2a32fa9e6b7cf0f6560b2
L <lockhartjasona@gmail.com>
2017-10-22 15:42:25 -0500

    old comment

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -2,11 +2,6 @@ import {combineReducers} from 'redux';
 
 import DevicesReducer from './DevicesReducer';
 
-/*
- * We combine all reducers into a single object before updated data is dispatched (sent) to store
- * Your entire applications state (store) is just whatever gets returned from all your reducers
- * */
-
 const allReducers = combineReducers({
     devices: DevicesReducer
 });
